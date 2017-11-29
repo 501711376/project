458b454bc82b1488b19817b6186eacc22b1f892d
The Deceased King <31958700+PAPAOWEN@users.noreply.github.com>
2017-11-29 12:40:35 -0500

    GJ today

--- Tianhao.py
+++ Tianhao.py
@@ -1,11 +1,24 @@
 mars=[12,23,34]
 pavo=[211,23,432]
 user=[0,0,0]
+xdis=locA[0]-locB[0]
+ydis=locA[1]-locB[1]
+zdis=locA[2]-locB[2]
+
 def calcdistance (locA, locB):
+    global xdis
     xdis=locA[0]-locB[0]
-    ydis=locA[1]-locB[1]
-    zdis=locA[2]-locB[2]
+    global ydis=locA[1]-locB[1]
+    global zdis=locA[2]-locB[2]
     locA= mars
     locB=user
+    
 
-totaldistance= ((xdis**2 + ydis**2 + zdis**2)**1/2
+choice=input("where do you want to go")
+
+calcdistance (0,0)
+
+if choice== "mars":
+    totaldistance=(xdis**2 + ydis**2 + zdis**2)**0.5
+elif choice=="pavo":
+    totaldistance=(xdis**2 + ydis**2 + zdis**2)**0.5
