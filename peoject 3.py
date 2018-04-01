73d3ede75c5956c93332c8224f4ae4aa0ddc7218
501711376 <31967587+501711376@users.noreply.github.com>
2018-03-31 20:13:10 -0400

    changes

--- peoject 3.py
+++ peoject 3.py
@@ -1,21 +1,103 @@
 import pygame
 from pygame.locals import *
+import sys, pygame
+pygame.init()
 
-SCREENRECT = Rect(0, 0, 700, 480)
-def main():
-    pygame.init()
-    winstyle=0
-    bestdepth = pygame.display.mode_ok(SCREENRECT.size,winstyle,32)
-    screen = pygame.display.set_mode(SCREENRECT.size,winstyle,bestdepth)
+import pygame
+import time
 
-    while True:
+pygame.init()
+
+display_width = 1000
+display_height = 600
+z= 1000
+c=600
+black = (0, 0, 0)
+white = (255, 255, 255)
+red = (255, 0, 0)
+
+car_width = 20
+
+gameDisplay = pygame.display.set_mode((display_width, display_height))
+pygame.display.set_caption('Star Trek')
+clock = pygame.time.Clock()
+
+carImg = pygame.image.load('ball.bmp')
+
+Marimg = pygame.image.load('mars.jpeg')
+
+def Mars(x,y):
+    gameDisplay.blit(Marimg, (x-130, y-400))
+
+def car(x, y):
+    gameDisplay.blit(carImg, (x, y))
+
+
+def text_objects(text, font):
+    textSurface = font.render(text, True, black)
+    return textSurface, textSurface.get_rect()
+
+
+def message_display(text):
+    largeText = pygame.font.Font('freesansbold.ttf', 115)
+    TextSurf, TextRect = text_objects(text, largeText)
+    TextRect.center = ((display_width / 2), (display_height / 2))
+    gameDisplay.blit(TextSurf, TextRect)
+
+    pygame.display.update()
+
+    time.sleep(2)
+
+    game_loop()
+
+
+def crash():
+    message_display('You Crashed')
+
+
+def game_loop():
+    x = (display_width * 0.45)
+    y = (display_height * 0.8)
+
+    x_change = 0
+
+    gameExit = False
+
+    while not gameExit:
+
         for event in pygame.event.get():
-            if event.type == QUIT or (event.type == KEYDOWN and event.key == K_ESCAPE):
-                return
-        keystate = pygame.key.get_pressed()
-main()
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                quit()
+
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_LEFT:
+                    x_change = -5
+                if event.key == pygame.K_RIGHT:
+                    x_change = 5
+
+            if event.type == pygame.KEYUP:
+                if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+                    x_change = 0
+
+        x += x_change
+
+        gameDisplay.fill(white)
+        car(x, y)
+        Mars(z, c)
+
+
+
+        if x > display_width - car_width or x < 0:
+            crash()
+
+        pygame.display.update()
+        clock.tick(60)
+
+game_loop()
 pygame.quit()
 quit()
+
 mars = (12, 23, 34)
 pavo = (211, 23, 432)
 user = (0, 0, 0)
@@ -59,5 +141,4 @@ def navigate():
 
 
 navigate()
-
 
